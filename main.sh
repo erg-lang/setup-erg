9aa8a81736b59f18587b408fc027381db74f1d1e
Shunsuke Shibayama <sbym1346@gmail.com>
2022-11-07 15:09:26 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-curl -O -L https://github.com/mtshiba/ergup/raw/main/ergup.pyc
-python3 ergup.pyc
+filename=ergup`python3 -c "import sys; print('%d%d' % (sys.version_info.major, sys.version_info.minor))"`.pyc
+curl -O -L https://github.com/mtshiba/ergup/raw/main/bin/$filename
+grep -q "404" $filename && echo "not supported python version" || python3 $filename
+
 echo "/home/runner/.erg/bin" >> $GITHUB_PATH
 echo "ERG_PATH=/home/runner/.erg" >> $GITHUB_ENV
 echo done
