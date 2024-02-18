09427b7623f07bc5929ef1b2d6cdc65cccb5462f
Shunsuke Shibayama <sbym1346@gmail.com>
2024-02-19 01:50:37 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-curl -L https://github.com/mtshiba/ergup/raw/main/ergup.py | python3
-
 echo "/home/runner/.erg/bin" >> $GITHUB_PATH
 echo "ERG_PATH=/home/runner/.erg" >> $GITHUB_ENV
+
+curl -L https://github.com/mtshiba/ergup/raw/main/ergup.py | python3
+
 echo done
