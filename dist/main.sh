f131de0d8d80b90ec2f2656e3e6912a1462c7668
Shunsuke Shibayama <sbym1346@gmail.com>
2025-01-29 10:40:13 +0900

    Update main.sh

--- dist/main.sh
+++ dist/main.sh
@@ -6,6 +6,6 @@ echo "ERG_PATH=/home/runner/.erg" >> $GITHUB_ENV
 export PATH=$PATH:~/.erg/bin
 export ERG_PATH=~/.erg
 
-curl -L https://github.com/mtshiba/ergup/raw/main/ergup.py | python3 - $BUILD
+curl -L https://github.com/mtshiba/ergup/raw/main/ergup/ergup.py | python3 - $BUILD
 
 echo done
